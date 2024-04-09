bdd5bb264c781cd80bd0ea70f9c293f416c56b06
Ana Leite <126627823+alleite@users.noreply.github.com>
2024-04-09 16:12:27 -0300

    Update App.js

--- aulaPam2Mtec/appDolar/App.js
+++ aulaPam2Mtec/appDolar/App.js
@@ -15,18 +15,49 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      <Text style ={styles.container}></Text>
+      <Text style ={styles.titulo}>Conversor de dolár americano para real brasileiro</Text>
 
+
+<Text style={styles.texto}>Quantidade em dolár:</Text>
+<TextInput
+placeholder='Quantidade em dolár'
+style={styles.caixa}
+keyboardType={'numeric'}
+onChangeText={dolar => setDolar(dolar)}
+>
+</TextInput>
+
+<Button
+style={styles.botao}
+title='Converter para real'
+onPress={converDolar}
+>
+</Button>
+<Text style={styles.resul}>{real ? `Valor em real ${real} ` : '' }</Text> 
     </View>
   );
-  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#54ffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto: {
+    color: '#543',
+    fontSize: 15,
+    textAlign: 'center'
+  },
+  caixa:{
+    textAlign: 'center',
+    borderWidth: 4,
+    width:200,
+    height:45,
+    borderColor: '#444',
+    borderRadius: 10, 
+    fontSize:18
+  },
+  
 });
